3f3974e45e367cfe5433f4762f1b05bc8e291e85
Beno <103076866+benovojnikovic@users.noreply.github.com>
2022-08-17 06:26:38 +0000

    made a lot of files and changes

--- index.js
+++ index.js
@@ -1,5 +1,50 @@
-var http = require("http");
-http.createServer(function(req,res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Aloha world');
-}).listen(process.env.PORT || 3000);
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('view engine','ejs')
+
+//this will allow us to serve up static files, css, images, and JS.
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => {
+  var title = "My Home Page";
+  var heading = "My Website";
+  res.render('pages/index',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/about', (req, res) => {
+  var title = "Valorant";
+  var heading = "My Website"
+  res.render('pages/about',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/contact', (req, res) => {
+  var title = "Grand Theft Auto V";
+  var heading = "My Website"
+  res.render('pages/contact',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+app.get('/brain', (req, res) => {
+  var title = "Undertale";
+  var heading = "My Website"
+  res.render('pages/brain',{
+    'title':title,
+    'heading':heading
+  })
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
